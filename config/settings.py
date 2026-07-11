d78319f6ec091e608e8c0296f964380b3a642023
behruz171 <behruzzo662@gmail.com>
2026-07-11 15:58:28 +0500

    new

--- config/settings.py
+++ config/settings.py
@@ -75,7 +75,7 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
